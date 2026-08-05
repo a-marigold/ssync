32a4ca32f0f50701f82ae58d86cb9d3de5a4bcae
marigold <phelixgluy@gmail.com>
2026-08-05 23:42:59 +0500

    update: move 'Errors' from 'main.zig' to 'constants.zig'

--- src/main.zig
+++ src/main.zig
@@ -6,24 +6,10 @@ const process = std.process;
 const unicode = std.unicode;
 const builtin = @import("builtin");
 
+const constants = @import("constants.zig");
 const utils = @import("utils.zig");
 
-const Errors = struct {
-    pub const UNKNOWN_CMD = "Unknown command. Use 'ssync --help'.";
-
-    pub const GET_USER_DIR_PATH_FAIL = "Failed to get the user dir path.";
-};
-
-const HELP_TEXT =
-    \\commands:
-    \\  list                           List the created roots and paths to them in the system.
-    \\  add [root, source, dest]       'root' is name of a root folder to copy 'source' file to. Root is created if does not exit.
-    \\                                 'source' is path to a file in the system which is to be copied to 'dest'.
-    \\                                 'dest' is a path relative to 'root' to copy 'source' to.
-    \\  delete [root, ?file]           If file specified, delete the 'file' in 'root'.
-    \\                                 If only 'root' specified, delete the whole root (prompt is shown for safety).
-    \\  update [root, newSource, dest] Make 'dest' in 'root' track 'newSource' instead of the current.
-;
+const Errors = constants.Errors;
 
 pub fn main(init: process.Init.Minimal) !void {
     const environ = init.environ;
@@ -46,7 +32,9 @@ pub fn main(init: process.Init.Minimal) !void {
         const stdoutWriter = &stdout.interface;
 
         if (mem.eql(u8, command, "--help")) {
-            try utils.writeStdio(stdoutWriter, HELP_TEXT);
+            try utils.writeStdio(
+                stdoutWriter,
+            );
 
             process.exit(0);
         } else if (mem.eql(u8, command, "list")) {
