c9b1e2f6f168e54e9bf0bc8ee02f7009abc42bf5
marigold <phelixgluy@gmail.com>
2026-08-06 21:11:39 +0500

    delete: constants.zig

--- src/constants.zig
+++
@@ -1,6 +0,0 @@
-pub const Errors = struct {
-    pub const UNKNOWN_CMD = "Unknown command. Use 'ssync --help'.";
-
-    pub const GET_USER_DIR_PATH_FAIL =
-        "Failed to get the user dir path.";
-};
