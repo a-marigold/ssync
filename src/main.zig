1db97ffeaac0c262bd2b8ad571c61f4a92d3c407
marigold <phelixgluy@gmail.com>
2026-08-06 17:53:29 +0500

    fix: the first cmd argument was not skipped

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,4 @@
-pub const std = @import("std");
+const std = @import("std");
 const mem = std.mem;
 const heap = std.heap;
 const Io = std.Io;
@@ -7,6 +7,7 @@ const unicode = std.unicode;
 const builtin = @import("builtin");
 
 const constants = @import("constants.zig");
+
 const utils = @import("utils.zig");
 
 const OS = builtin.os.tag;
@@ -22,22 +23,24 @@ pub fn main(init: process.Init.Minimal) !void {
     var threaded: Io.Threaded = .init(arenaAllocator, .{});
     const io = threaded.io();
 
+    // Empty slice for unbuffered io
     var stderr = Io.File.stdout().writer(io, &.{});
-
     const stderrWriter = &stderr.interface;
 
     var args = try init.args.iterateAllocator(arenaAllocator);
 
-    if (args.next()) |command| {
-        var stdout = Io.File.stdout().writer(io, &.{});
+    _ = args.skip();
 
+    if (args.next()) |cmd| {
+        // Empty slice for unbuffered io
+        var stdout = Io.File.stdout().writer(io, &.{});
         const stdoutWriter = &stdout.interface;
 
-        if (mem.eql(u8, command, "--help")) {
+        if (mem.eql(u8, cmd, "--help")) {
             try utils.writeStdio(stdoutWriter, Commands.help());
 
             process.exit(0);
-        } else if (mem.eql(u8, command, "list")) {
+        } else if (mem.eql(u8, cmd, "list")) {
             const listOutput = try Commands.list(arenaAllocator, io, environ);
 
             try utils.writeStdio(stdoutWriter, listOutput.items);
@@ -49,7 +52,21 @@ pub fn main(init: process.Init.Minimal) !void {
     }
 }
 
+/// The CLI commands.
 const Commands = struct {
+    pub inline fn help() []const u8 {
+        return
+        \\commands:
+        \\  list                           List the created roots and paths to them in the system.
+        \\  add [root, source, dest]       'root' is name of a root folder to copy 'source' file to. Root is created if does not exit.
+        \\                                 'source' is path to a file in the system which is to be copied to 'dest'.
+        \\                                 'dest' is a path relative to 'root' to copy 'source' to.
+        \\  delete [root, ?file]           If file specified, delete the 'file' in 'root'.
+        \\                                 If only 'root' specified, delete the whole root (prompt is shown for safety).
+        \\  update [root, newSource, dest] Make 'dest' in 'root' track 'newSource' instead of the current.
+        ;
+    }
+
     pub inline fn list(allocator: mem.Allocator, io: Io, environ: process.Environ) !std.ArrayList(u8) {
         var output: std.ArrayList(u8) = try .initCapacity(allocator, 600);
 
@@ -96,17 +113,5 @@ const Commands = struct {
         }
 
         return output;
-    }
-    pub inline fn help() []const u8 {
-        return
-        \\commands:
-        \\  list                           List the created roots and paths to them in the system.
-        \\  add [root, source, dest]       'root' is name of a root folder to copy 'source' file to. Root is created if does not exit.
-        \\                                 'source' is path to a file in the system which is to be copied to 'dest'.
-        \\                                 'dest' is a path relative to 'root' to copy 'source' to.
-        \\  delete [root, ?file]           If file specified, delete the 'file' in 'root'.
-        \\                                 If only 'root' specified, delete the whole root (prompt is shown for safety).
-        \\  update [root, newSource, dest] Make 'dest' in 'root' track 'newSource' instead of the current.
-        ;
     }
 };
