fda14029b4085a7ce2b4e725068fedeabda52cc8
marigold <phelixgluy@gmail.com>
2026-08-16 15:49:54 +0500

    add(build.zig): 'asm' step

--- build.zig
+++ build.zig
@@ -1,22 +1,32 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // const installStep = b.getInstallStep();
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const isRelease = optimize == .fast or optimize == .small;
 
-    const exe = b.addExecutable(.{ .name = "ssync", .root_module = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+    const exe = b.addExecutable(.{
+        .name = "ssync",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
 
-        .single_threaded = true,
-        .strip = isRelease,
-        .stack_check = !isRelease,
-        .omit_frame_pointer = isRelease,
-        .unwind_tables = if (isRelease) .none else null,
-    }) });
+            .single_threaded = true,
+            .strip = isRelease,
+            .stack_check = !isRelease,
+            .omit_frame_pointer = isRelease,
+            .unwind_tables = if (isRelease) .none else null,
+        }),
+        // Exactly it allows emitting asm for some reason
+        .use_llvm = true,
+    });
+
+    const asmStep = b.step("asm", "Emit asm");
+    asmStep.dependOn(&b.addInstallBinFile(exe.getEmittedAsm(), "ssync.s").step);
 
     const checkStep = b.step("check", "Check without emiting binary");
     checkStep.dependOn(&exe.step);
